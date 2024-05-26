7bed9fd1516ec66ff0e3a53be63fb6e4efeb1e65
Vib-UX <vibchess123@gmail.com>
2024-05-26 23:26:02 +0530

    cors fix

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require('./routes/routes');  
+const cors = require('cors');
 
 // Middleware
 app.use(bodyParser.json());
-
+app.use(cors())
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
